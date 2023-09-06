55d5708b0221e349111e41833a979636b58ac1a3
Het Patel <106577127+Het01@users.noreply.github.com>
2023-09-06 23:29:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 5000;
+
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 
 const mongoDB = require('./db');
 mongoDB();
