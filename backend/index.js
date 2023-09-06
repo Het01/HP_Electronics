a58d3927479b12bfc9f35b429f43c53071f4a499
Het Patel <106577127+Het01@users.noreply.github.com>
2023-09-06 23:49:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const port = 5000;
 
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://hp-electronics-frontend.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
